2ae57caafff8ade947d040de740ecd694a43d8ec
sanjay-rathor <sanjay.rathor@thegrowthtrack.com>
2024-03-27 17:26:39 +0530

    link replace home page vol 4

--- src/views/home/landingJournal.js
+++ src/views/home/landingJournal.js
@@ -124,7 +124,7 @@ export default function LandingJournal() {
                         <Card.Title>
                           <a
                             href={
-                              "/browse-journal-vol-3-issue-2#" + article.title
+                              "/browse-journal-vol-4-issue-1#" + article.title
                             }
                           >
                             {article.title}
