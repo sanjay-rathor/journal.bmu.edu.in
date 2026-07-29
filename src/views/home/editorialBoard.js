9c762825cd32a55acfeccb6672e06842d30c08ea
Devraj <devrajk238@gmail.com>
2026-07-29 17:16:41 +0530

    alphabetical labels again

--- src/views/home/editorialBoard.js
+++ src/views/home/editorialBoard.js
@@ -41,8 +41,7 @@ export default function EditorialBoard() {
                       <div className=" list-group-item flex-column align-items-start ">
                         <div className="d-flex w-100 justify-content-between name-box">
                           <h5 className="mb-1">
-                            {index + 1}. {item.name}
-                            </h5>
+                            {item.name}</h5>
                         </div>
                         <p className="mb-1 editor-about">{item.title}</p>
                         <p className="mb-1 editor-about">{item.email}</p>
